4002773486dc2f6a2eac07755c9f26bc3872d5fb
Antony Valete <antonyvalete@gmail.com>
2023-05-30 15:36:02 -0300

    Add Jewel class and inherting it from gameObject

--- jewel_collector_2.0/Jewel.cs
+++ jewel_collector_2.0/Jewel.cs
@@ -1,6 +1,10 @@
 namespace JewelCollector
 {
-    public class RedJewel : gameObject
+    public class Jewel : gameObject
+    {
+        public Jewel(int x, int y, bool passable, bool collectable) : base(x, y, passable, collectable) { }
+    }
+    public class RedJewel : Jewel
     {
         public static readonly string displayName = "RJ";
         public static readonly int value = 100;
@@ -11,13 +15,13 @@ namespace JewelCollector
             return RedJewel.displayName;
         }
     }
-    public class GreenJewel : gameObject
+    public class GreenJewel : Jewel
     {
         public static readonly string displayName = "GJ";
         public static readonly int value = 50;
         public GreenJewel(int x, int y, bool passable, bool collectable) : base(x, y, passable, collectable) { }
     }
-    public class BlueJewel : gameObject
+    public class BlueJewel : Jewel
     {
         public static readonly string displayName = "BJ";
         public static readonly int value = 10;
