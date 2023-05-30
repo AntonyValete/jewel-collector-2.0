c4b35bcf40ef2bc8ca5ba9cd7926c0ac1ba7e2cb
Antony Valete <antonyvalete@gmail.com>
2023-05-30 10:00:51 -0300

    Adding Coordinate class

---
+++ jewel_collector_2.0/Coordinate.cs
@@ -0,0 +1,15 @@
+namespace JewelCollector {
+    public class Coordinate {
+        private int x {get; set;}        
+        private int y {get; set;}
+
+        public Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public (int, int) getCoordinate() {
+            return (this.x, this.y);
+        }
+    }
+}
