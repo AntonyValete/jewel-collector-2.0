0ff17ad4f4a211857d0d965d1672ec1a2723b94b
Antony Valete <antonyvalete@gmail.com>
2023-06-01 19:24:33 -0300

    Adding: creating a matrix attribute of gameObject, swap function, Map class constructor, and updating the Print method to be an instance method

--- jewel_collector_2.0/Map.cs
+++ jewel_collector_2.0/Map.cs
@@ -1,21 +1,36 @@
-namespace JewelCollector {
-    public class map {
+namespace JewelCollector
+{
+    //It would be great if we could implement the singleton design pattern, but don't bother with it. Map and Robot
+    public class Map
+    {
+        private gameObject[,] map;
 
-        //TODO
-        private void Swap(gameObject A, gameObject B) {
+        private Robot robot;
 
+        public Map(Robot robot)
+        {
+            this.map = new gameObject[3, 3];
+            this.robot = robot;
         }
-        public static void Print(string[,] mapArray, Robot robot) {
 
-            (int x, int y) = robot.getCoordinate();
-            mapArray[y, x] = Robot.displayName;
+        //This method need to be used within another method in order to change the position of the robot.
+        private void Swap(ref gameObject A, ref gameObject B)
+        {
+            gameObject temp = A;
+            A = B;
+            B = temp;
+        }
 
-            for (int i = 0; i < mapArray.GetLength(0); i++) {
-                for (int j = 0; j < mapArray.GetLength(1); j++) {
-                    Console.Write(mapArray[i,j] + " ");
+        public void Print()
+        {
+            for (int i = 0; i < this.map.GetLength(0); i++)
+            {
+                for (int j = 0; j < this.map.GetLength(1); j++)
+                {
+                    Console.Write(this.map[i, j].getDisplayName());
                 }
-                Console.Write("\n");
-            }  
+                Console.WriteLine();
+            }
         }
     }
 }
