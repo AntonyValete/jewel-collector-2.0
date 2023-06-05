f0737cecb82f0e79f13a7cdcf586551b20fd8d39
Antony Valete <antonyvalete@gmail.com>
2023-06-05 16:45:41 -0300

    Update: using C# brakets convention

--- jewel_collector_2.0/Robot.cs
+++ jewel_collector_2.0/Robot.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace JewelCollector {
-    public class Robot : gameObject {   
+namespace JewelCollector
+{
+    public class Robot : gameObject
+    {
         public static readonly string displayName = "ME";
         public uint totalEnergy;
         public (int cx, int cy) coordCache;
         private (int dx, int dy) coordTemp;
-        public Robot(int x, int y, bool passable, bool collectable, uint totalEnergy) : base(x, y, passable, collectable) {
+        public Robot(int x, int y, bool passable, bool collectable, uint totalEnergy) : base(x, y, passable, collectable)
+        {
             setCoordinate(x, y);
             this.totalEnergy = totalEnergy;
         }
-        
+
         // TODO: Exception handler:
-        public bool checkIfEmpty(int x, int y) {
+        public bool checkIfEmpty(int x, int y)
+        {
             // Handling out of bounds check
             if (x < 0 || y < 0) return false;
             if (x > 9 || y > 9) return false;
@@ -25,25 +29,28 @@ namespace JewelCollector {
             return true;
         }
 
-        public void Movement(ConsoleKey key, ref Map map) { // método de movimento vai mudar as coordenadas dependendo da tecla pressionada
-            
+        public void Movement(ConsoleKey key, ref Map map)
+        { // método de movimento vai mudar as coordenadas dependendo da tecla pressionada
+
             (int x, int y) = this.getCoordinate();
             (int dx, int dy) coordTemp = this.getCoordinate();
-            switch (key) {
+            switch (key)
+            {
                 case (ConsoleKey.A):
-                    coordTemp = (x-1, y);
+                    coordTemp = (x - 1, y);
                     break;
                 case (ConsoleKey.D):
-                    coordTemp = (x+1, y);
+                    coordTemp = (x + 1, y);
                     break;
                 case (ConsoleKey.S):
-                    coordTemp = (x, y+1);
+                    coordTemp = (x, y + 1);
                     break;
                 case (ConsoleKey.W):
-                    coordTemp = (x, y-1);
+                    coordTemp = (x, y - 1);
                     break;
             }
-            if (checkIfEmpty(coordTemp.dx, coordTemp.dy) == true) {
+            if (checkIfEmpty(coordTemp.dx, coordTemp.dy) == true)
+            {
                 coordCache = (x, y);
                 setCoordinate(coordTemp.dx, coordTemp.dy);
                 this.totalEnergy--;
