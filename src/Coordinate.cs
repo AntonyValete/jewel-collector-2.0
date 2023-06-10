0c62874dbe61a3d6b10976a708ee92aa6fb87a3f
Antony Valete <antonyvalete@gmail.com>
2023-06-10 20:22:53 -0300

    Update: changing folder name from "jewel_collector_2.0" to "src"

---
+++ src/Coordinate.cs
@@ -0,0 +1,25 @@
+namespace JewelCollector
+{
+    public class Coordinate
+    {
+        private int x { get; set; }
+        private int y { get; set; }
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public (int, int) getCoordinate()
+        {
+            return (this.x, this.y);
+        }
+
+        public void setCoordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
